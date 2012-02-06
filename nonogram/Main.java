0cab058b7e6551ea5206bcaec88aa024a6e9cf61
Sam Magura <srmagura@gmail.com>
2012-02-06 12:14:57 -0500

    Move root of git repo to include package nonogram

---
+++ nonogram/Main.java
@@ -0,0 +1,27 @@
+package nonogram;
+
+import java.io.*;
+
+public class Main {
+    
+    public Main(){
+        try {
+
+        BufferedReader br = new BufferedReader( 
+                new InputStreamReader(
+                this.getClass().getResourceAsStream("puzzles/test1.dat")));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            Nonogram nonogram = Nonogram.fromFile(line);
+        }
+
+        } catch (IOException e) {
+            System.err.println("File not found");
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+}
