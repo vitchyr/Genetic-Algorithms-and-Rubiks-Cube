b9dd5d1e270e16c6e4fecf8edad9472cf81ec34a
Sam Magura <srmagura@gmail.com>
2012-02-10 08:45:49 -0500

    generations, .etc squash

--- nonogram/Main.java
+++ nonogram/Main.java
@@ -5,23 +5,104 @@ import nonogram.gui.Window;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
+    ArrayList<Solution> population;
+    Random random;
+    int POP_SIZE = 50;
+    int ELITE_SIZE = 2;
+
     public Main() {
         Nonogram nonogram = nonogramFromFile("puzzles/test1.dat");
+        random = new Random();
 
         Window window = new Window();
         window.setNonogram(nonogram);
+
+        for (int i = 0; i < POP_SIZE; i++) {
+            Solution solution = new Solution(nonogram);
+            solution.generateSol();
+
+            population.add(solution);
+        }
+
+        doGeneration();
     }
 
-    public Nonogram nonogramFromFile(String path) {
-        Nonogram nonogram = null;
+    public void doGeneration() {
+        for (Solution solution : population) {
+            solution.evaluate();
+        }
+
+        Collections.sort(population);
+
+        ArrayList<Solution> newPopulation = new ArrayList();
+
+        //Elitism
+        for (int i = 0; i < ELITE_SIZE; i++) {
+            if(population.get(i).getFitness() == 1){
+                System.out.println("Done");
+            }
+            
+            newPopulation.add(population.get(i));
+            population.remove(i);
+        }
+
+        int[] weights = new int[population.size()];
+        for (int i = 0; i < population.size(); i++) {
+            weights[i] = population.get(i).getFitness();
+        }
+
+
+        crossover:
+        while (true) {
+            Solution parent1 = population.get(getWeightedRandom(weights));
+            Solution parent2 = population.get(getWeightedRandom(weights));
+
+            Solution[] offspring = parent1.crossover(parent2, random.nextFloat());
+
+            for (int i = 0; i < 2; i++) {
+                newPopulation.add(offspring[i]);
+                
+                if(newPopulation.size() == POP_SIZE - ELITE_SIZE){
+                    break crossover;
+                }
+            }
+        }
         
+        
+    }
+
+    public int getWeightedRandom(int[] weights) {
+        float[] runningTotals = new float[weights.length];
+
+        runningTotals[0] = weights[0];
+        for (int i = 1; i < weights.length; i++) {
+            runningTotals[i] = runningTotals[i - 1] + (float) weights[i];
+        }
+
+        float randomNumber = random.nextFloat();
+
+        for (int i = 0; i < weights.length; i++) {
+            if (randomNumber < runningTotals[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static Nonogram nonogramFromFile(String path) {
+        Nonogram nonogram = null;
+
         try {
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(
-                    this.getClass().getResourceAsStream(path)));
+                    Main.class.getResourceAsStream(path)));
 
             String line;
             while ((line = br.readLine()) != null) {
@@ -30,8 +111,8 @@ public class Main {
 
         } catch (IOException e) {
             System.err.println("File not found");
-        }  
-        
+        }
+
         return nonogram;
     }
 
--- nonogram/Solution.java
+++ nonogram/Solution.java
@@ -2,40 +2,57 @@ package nonogram;
 
 import java.util.Random;
 
-public class Solution{
+public class Solution implements Comparable<Solution> {
+
     private boolean[][] potentialSol;
     private int rNum, cNum;
     private Nonogram nonogram;
-    
-    public Solution(Nonogram nonogram){        
+
+    public Solution(Nonogram nonogram) {
         rNum = nonogram.getRowHeaders().size();
         cNum = nonogram.getColumnHeaders().size();
         this.nonogram = nonogram;
-        
+
         potentialSol = new boolean[rNum][cNum];
-        generateSol();
     }
-    
-    public void generateSol(){
+
+    public void generateSol() {
         Random randGen = new Random();
-        
-        for (boolean[] array : potentialSol){
-            for (boolean block : array){
+
+        for (boolean[] array : potentialSol) {
+            for (boolean block : array) {
                 block = randGen.nextBoolean();
             }
-        }        
+        }
     }
-    
-    
+
+    //work to do
+    public Solution[] crossover(Solution solution, float random) {       
+        Solution[] offspring = new Solution[2];
+
+        for (int i = 0; i < this.potentialSol.length; i++) {
+            for (int j = 0; j < this.potentialSol[i].length; j++) {
+                offspring[0].getPotentialSol()[i][j] = this.potentialSol[i][j];
+                offspring[1].getPotentialSol()[i][j] = 
+                    solution.getPotentialSol()[i][j];
+            }
+        }
+        
+        return offspring;
+    }
+
+    public int compareTo(Solution solution) {
+        return this.fitness - anotherInstance.fitness;
+    }
     //public int evaluate(Nonogram ng){
-        //for(row :
+    //for(row :
     //}
 
     public Nonogram getNonogram() {
         return nonogram;
     }
-    
-    public boolean[][] getPotentialSol(){
+
+    public boolean[][] getPotentialSol() {
         return potentialSol;
     }
 }
