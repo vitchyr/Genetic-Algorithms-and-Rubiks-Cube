a8c0df4e373de4628a929991bee26bd01e36b93b
Ada Taylor <ada@idu.com>
2012-02-13 01:17:17 -0500

    Changed action listener to if else format, so correct format

--- nonogram/gui/Window.java
+++ nonogram/gui/Window.java
@@ -10,84 +10,81 @@ import nonogram.*;
 
 public class Window extends JFrame implements ActionListener {
 
-    Main main;
-    final Canvas canvas;
-    final JLabel label;
-    JPanel panel, buttonPanel;
-    JButton startButton, loadButton;
-    JTextField textfield;
+	Main main;
+	final Canvas canvas;
+	final JLabel label;
+	JPanel panel, buttonPanel;
+	JButton startButton, loadButton;
+	JTextField textfield;
 
-    public Window(Main main) {
-        super("Nonogram Genetic Algorithm");
-        this.main = main;
-        
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public Window(Main main) {
+		super("Nonogram Genetic Algorithm");
+		this.main = main;
 
-        canvas = new Canvas();
-        getContentPane().add(canvas, BorderLayout.CENTER);
-        
-        panel = new JPanel();
-        getContentPane().add(panel, BorderLayout.SOUTH);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        label = new JLabel("Press start to begin");
-        panel.add(label, BorderLayout.CENTER);
-        
-        buttonPanel = new JPanel();               
-        buttonPanel.setPreferredSize(new Dimension(400, 50));
-        panel.add(buttonPanel, BorderLayout.SOUTH);
-        
-        startButton = new JButton("Start");
-        startButton.addActionListener(this);
-        startButton.setActionCommand("start");
-        buttonPanel.add(startButton, BorderLayout.WEST);
-        
-        loadButton = new JButton("Load:");
-        buttonPanel.add(loadButton, BorderLayout.CENTER);
-        
-        textfield = new JTextField(10);
-        buttonPanel.add(textfield, BorderLayout.CENTER);
+		canvas = new Canvas();
+		getContentPane().add(canvas, BorderLayout.CENTER);
 
-        setPreferredSize(new Dimension(400, 400));
-        pack();
-        setVisible(true);
-    }
+		panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.SOUTH);
 
-    public void setNonogram(Nonogram nonogram) {
-        canvas.setNonogram(nonogram);
-        pack();
-    }
-    
-    public Nonogram getNonogram(){
-        return canvas.getNonogram();
-    }
+		label = new JLabel("Press start to begin");
+		panel.add(label, BorderLayout.CENTER);
 
-    public void setSolution(Solution solution) {
-        assert (solution.getNonogram() == canvas.getNonogram());
-        canvas.setSolution(solution);
-    }
+		buttonPanel = new JPanel();
+		buttonPanel.setPreferredSize(new Dimension(400, 50));
+		panel.add(buttonPanel, BorderLayout.SOUTH);
 
-    public void setLabelText(int gen, int fitness) {
-        label.setText("Generation: " + Integer.toString(gen) + "   " +
-            "Highest fitness: " + Integer.toString(fitness));
+		startButton = new JButton("Start");
+		startButton.addActionListener(this);
+		startButton.setActionCommand("start");
+		buttonPanel.add(startButton, BorderLayout.WEST);
 
-        pack();
-    }
+		loadButton = new JButton("Load:");
+		buttonPanel.add(loadButton, BorderLayout.CENTER);
 
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        switch (event.getActionCommand()) {
-            case "start":
-                main.startWorker(canvas.getSolution());
-                startButton.setText("Stop");
-                startButton.setActionCommand("stop");
-                break;
-            case "stop":
-                main.cancelWorker();
-                startButton.setText("Start");
-                startButton.setActionCommand("start");
-                break;
-            case "load":
-                break;
-        }
-    }
+		textfield = new JTextField(10);
+		buttonPanel.add(textfield, BorderLayout.CENTER);
+
+		setPreferredSize(new Dimension(400, 400));
+		pack();
+		setVisible(true);
+	}
+
+	public void setNonogram(Nonogram nonogram) {
+		canvas.setNonogram(nonogram);
+		pack();
+	}
+
+	public Nonogram getNonogram() {
+		return canvas.getNonogram();
+	}
+
+	public void setSolution(Solution solution) {
+		assert (solution.getNonogram() == canvas.getNonogram());
+		canvas.setSolution(solution);
+	}
+
+	public void setLabelText(int gen, int fitness) {
+		label.setText("Generation: " + Integer.toString(gen) + "   "
+				+ "Highest fitness: " + Integer.toString(fitness));
+
+		pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if (event.getActionCommand().equals("start")) {
+
+			main.startWorker(canvas.getSolution());
+			startButton.setText("Stop");
+			startButton.setActionCommand("stop");
+		} else if (event.getActionCommand().equals("stop")) {
+			main.cancelWorker();
+			startButton.setText("Start");
+			startButton.setActionCommand("start");
+		} else if (event.getActionCommand().equals("load")) {
+		}
+	}
 }
